6acfd183b3f34e1bd9810b3fb90ebd2f4a9f9d05
Pallavi2911 <>
2023-02-28 12:08:16 +0900

    Error Handling

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -13,15 +13,21 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe((posts) => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
@@ -31,10 +37,15 @@ export class AppComponent implements OnInit {
   onFetchPosts() {
     this.isFetching = true;
     // Send Http request
-    this.postsService.fetchPosts().subscribe((posts) => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
   }
 
   onClearPosts() {
